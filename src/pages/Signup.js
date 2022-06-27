f37f0281683e07a3b4f944703a8308d91d2aa8e8
Woojung0618 <woojung0618@gmail.com>
2022-06-28 00:23:54 +0900

    FEAT: Sign up 페이지 퍼블리싱

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
 
 const Wrapper = styled.div`
     .detail-text {
@@ -10,8 +9,30 @@ const Wrapper = styled.div`
 `
 
 const Signup = () => {
+    const [details, setDetails] = useState({
+        id: "",
+        password: "",
+        password2: "",
+        nickname: "",
+    })
     const [idCheck, setIdCheck] = useState(null);
-    const [pwCheck, setPwCheck] = useState(null);
+    const [nameCheck, setNameCheck] = useState(null);
+
+    const clickSignupBtn = () => {
+        if (idCheck !== true || nameCheck !== true || details.password !== details.password2) {
+            alert('입력칸을 모두 작성하고 체크를 완료해주세요.');
+        } else {
+            // postAPI 회원가입 
+        }
+    }
+
+    const clickCheckBtn = (c) => {
+        if (c === 'id') {
+            // id 중복체크 getAPI
+        } else {
+            // nickname 중복체크 getAPI
+        }
+    }
 
     return (
         <Wrapper>
@@ -20,12 +41,12 @@ const Signup = () => {
             <form class="container col-lg-4 col-sm-6 col-10" >
                 <div class="row justify-content-center" style={{ marginRight: '0' }}>
                     <div class="form-outline mb-4 col-10">
-                        <input type="text" id="form2Example1" class="form-control" placeholder="아이디" />
+                        <input type="text" class="form-control" placeholder="아이디" />
                     </div>
                     <button
-                        type="button"
-                        class="btn btn-primary btn-block mb-4 col-2"
-                        style={{ fontSize: '12px', whiteSpace: 'nowrap' }}>
+                        type="button" class="btn btn-secondary btn-block mb-4 col-2"
+                        style={{ fontSize: '12px', whiteSpace: 'nowrap' }}
+                        onClick={() => clickCheckBtn('id')}>
                         check
                     </button>
                 </div>
@@ -34,24 +55,26 @@ const Signup = () => {
                         idCheck ? (
                             <p className="detail-text">사용가능한 아이디입니다.</p>
                         ) : (
-                            <p className="detail-text">중복된 아이디입니다. 다른 아이디를 입력해주세요.</p>
+                            <p className="detail-text">이미 사용중인 아이디입니다. 다른 아이디를 입력해주세요.</p>
                         )
                     )
                 }
                 <div class="row justify-content-center">
                     <div class="form-outline mb-4">
-                        <input type="password" id="form2Example2" class="form-control" placeholder="비밀번호" />
+                        <input type="password" class="form-control" placeholder="비밀번호"
+                            onChange={(e) => setDetails({ ...details, password: e.target.value})} />
                     </div>
                 </div>
 
                 <div class="row justify-content-center">
                     <div class="form-outline mb-4">
-                        <input type="password" id="form2Example2" class="form-control" placeholder="비밀번호 확인" />
+                        <input type="password" class="form-control" placeholder="비밀번호 확인" 
+                            onChange={(e) => setDetails({ ...details, password2: e.target.value})}/>
                     </div>
                 </div>
                 {
-                    pwCheck !== null && (
-                        pwCheck ? (
+                    details.password2 !== "" && (
+                        details.password2 === details.password ? (
                             <p className="detail-text">비밀번호가 일치합니다.</p>
                         ) : (
                             <p className="detail-text">비밀번호가 일치하지 않습니다.</p>
@@ -59,11 +82,31 @@ const Signup = () => {
                     )
                 }
 
-                <div class="form-outline mb-4">
-                    <input type="text" id="form2Example2" class="form-control" placeholder="닉네임" />
+                <div class="row justify-content-center" style={{ marginRight: '0' }}>
+                    <div class="form-outline mb-4 col-10">
+                        <input type="text" class="form-control" placeholder="닉네임" />
+                    </div>
+                    <button
+                        type="button" class="btn btn-secondary btn-block mb-4 col-2"
+                        style={{ fontSize: '12px', whiteSpace: 'nowrap' }}
+                        onClick={() => clickCheckBtn('nickname')}>
+                        check
+                    </button>
                 </div>
+                {
+                    idCheck !== null && (
+                        idCheck ? (
+                            <p className="detail-text">사용가능한 닉네임입니다.</p>
+                        ) : (
+                            <p className="detail-text">이미 사용중인 닉네임입니다. 다른 닉네임을 입력해주세요.</p>
+                        )
+                    )
+                }
 
-                <button type="button" class="btn btn-primary btn-block mb-4 col-12">회원가입</button>
+                <button type="button" class="btn btn-primary btn-block mb-4 col-12"
+                    onClick={() => clickSignupBtn()}>
+                    회원가입
+                </button>
             </form>
         </Wrapper>
     )
